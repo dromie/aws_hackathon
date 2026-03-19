bbf05e5335bcd12eb2ca2b14fd2251712648da3d
Workshop Participant <participant@example.com>
2026-03-19 10:32:53 +0000

    Wider map

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ BASE = os.path.dirname(os.path.abspath(__file__))
 # --- Simulation constants ---
 RALLY      = (47.4871, 19.0707)  # Nokia Skypark
 BOUNDS_LAT = (47.479, 47.496)
-BOUNDS_LNG = (19.058, 19.080)
+BOUNDS_LNG = (19.040, 19.100)
 WANDER_SEC = 6
 NUM_GROUPS = 12
 TICK_SEC   = 0.033  # ~30 fps
